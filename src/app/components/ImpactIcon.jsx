f72a0737c8e8a8301bfb0169510b7208ba92041a
OssieSchie <osvald.rye@gmail.com>
2024-04-24 18:06:56 +0200

    giver op for nu

--- src/app/components/ImpactIcon.jsx
+++ src/app/components/ImpactIcon.jsx
@@ -2,27 +2,15 @@ import React from 'react'
 
 
 export default function ImpactIcon(props) {
-
     function CheckImpact(props, impact){
-        if(props === impact){
+        if(props == impact){
             return (<div impact="minor" className='border-2 border-slate-700 rounded-full'>
-                <p>severity {props}</p>
+                {impact}
             </div>)
         } else{
             return null;
         }
     }
-    // function CheckImpact(props){
-    //     if(props === "minor"){
-    //         return <div>Minor</div>
-    //     } else if (props === "moderate"){
-    //         return <div>Moderate</div>
-    //     } else if (props === "serious"){
-    //         return <div>Serious</div>
-    //     } else if (props === "critical"){
-    //         return <div>Critical</div>
-    //     }
-    // }
 
   return (
     <div>
