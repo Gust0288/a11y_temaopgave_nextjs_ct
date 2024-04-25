285cacc47c00956135a94bc6841a0ff67cefcde4
Ossie <osvald.rye@gmail.com>
2024-04-25 12:57:14 +0200

    flidt ui halløj

--- src/app/report/page.jsx
+++ src/app/report/page.jsx
@@ -5,6 +5,7 @@ import data from "../../../public/examples/kea_dk";
 // import data from "../../assets/examples/kea_dk.json";
 import Image from "next/image";
 import ReportExpand from "../components/ReportExpand";
+import "../globals.css";
 
 // //Fetch API virker med raport
 // export const revalidate = 1800;
@@ -23,14 +24,13 @@ export default function Page() {
   //slet og erstat med working API når færdig med styling/setup
   return (
     <main className="max-w-5xl m-auto grid grid-cols-1 my-5">
-      <div className="grid md:grid-cols-2">
-        <div className="border-2 flex-col">
-          <p>
-            Report for:
-            <br />
+      <div className="grid md:grid-cols-2 mb-12">
+        <div className="flex flex-col">
+          <div>
+            <p>Report for:</p>
             <h1>{data.url}</h1>
-          </p>
-          <h4>Report filed at: {data.timestamp}</h4>
+          </div>
+          <h4 className="mt-auto">Report filed at: {data.timestamp}</h4>
         </div>
 
         <Image
