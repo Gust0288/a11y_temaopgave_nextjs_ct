4d0d7a847f58ee72794eb3a36ccbd25bdf383e61
Ossie <osvald.rye@gmail.com>
2024-04-30 10:37:10 +0200

    giver op på score for nu

--- src/app/report/page.jsx
+++ src/app/report/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import data from "../../../public/examples/kea_dk";
+import data from "../../../public/examples/dsb_dk";
+// import data from "../../../public/examples/kea_dk";
 import Image from "next/image";
 import ReportExpand from "../components/ReportExpand";
 import "../globals.css";
@@ -17,12 +18,29 @@ import "../globals.css";
 //   console.log("data", data);
 //   console.log("data", data.violations);
 
+// calc total checks
 const totalViolations = data.violations.length;
 const totalPasses = data.passes.length;
 
 const totalChecks = totalPasses + totalViolations;
 
+// Calc score
+const violations = data.violations;
+
+function impactWeight(violations, impact) {
+  return violations.filter((el) => el.includes(impact));
+}
+
+const moderateImpact = impactWeight(data.violations, "moderate");
+
+//const minorImpact = data.violations.filter((minor));
+//const moderateImpact = data.violations.filter("moderate");
+
+// "minor", "moderate", "serious", or "critical"
+
 export default function Page() {
+  console.log(minorImpact);
+  console.log(moderateImpact);
   //slet og erstat med working API når færdig med styling/setup
   return (
     <main className="max-w-5xl m-auto grid grid-cols-1 my-5">
@@ -44,12 +62,20 @@ export default function Page() {
           className="border-2 border-slate-800  w-full md:w-1/2 xl:w-[600px]"
         />
       </div>
-      <h2 className="bg-brand-grey-80 text-brand-grey-00 pl-2">{totalChecks} relevant points checked</h2>
+      <h2 className="bg-brand-grey-80 text-brand-grey-00 pl-2">
+        {totalChecks} relevant points checked
+      </h2>
       {/* skal den vøre fold-out ligesom reportExpand? */}
-      <h3 className="pl-2 text-brand-grey-80">{data.passes.length} points cleared succesfully</h3>
+      <h3 className="pl-2 text-brand-grey-80">
+        {data.passes.length} points cleared succesfully
+      </h3>
       <div className="flex gap-2 mt-10">
-      <h3 className="font-semibold text-brand-grey-100">Found {data.violations.length} issues</h3>
-      <h3 className="px-4 font-semibold text-brand-grey-00 bg-brand-grey-80 rounded-full">!</h3>
+        <h3 className="font-semibold text-brand-grey-100">
+          Found {data.violations.length} issues
+        </h3>
+        <h3 className="px-4 font-semibold text-brand-grey-00 bg-brand-grey-80 rounded-full">
+          !
+        </h3>
       </div>
       {data.violations.map((violation) => (
         <ReportExpand key={violation.id} {...violation} />
