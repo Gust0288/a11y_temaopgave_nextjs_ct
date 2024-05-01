0e68c48deb4665b50481360574fc3647080d61a0
Gustav <gubo0001@stud.kea.dk>
2024-05-01 11:34:35 +0200

    Refactor Header component to include Diversa logo and update styling

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -7,20 +7,20 @@ function Header() {
   return (
     <nav className="">
       <ul className="flex justify-center items-center gap-2 md:gap-5  p-3 max-w-full md:max-w-96 rounded-lg mx-auto">
-        <li tabIndex="1">
+        <li>
           <Link
             href={"/"}
-            className="-mr-10 px-2 border-2 rounded-md w-25 md:w-40 lg:w-xl text-center border-brand-turquoise-50 bg-brand-grey-00 text-brand-orange-70"
+            className=" px-2 border-2 rounded-md w-25 md:w-40 lg:w-xl text-center border-brand-turquoise-50 bg-brand-grey-00 text-brand-orange-70"
           >
             Home
           </Link>
         </li>
-        <Image src={diversaLogo} alt="Diversa logo" className="h-20" />
+        <Image src={diversaLogo} alt="Diversa logo" className="w-auto h-20" />
         {/* ^Erstat med diversea logo^ */}
-        <li tabIndex="0">
+        <li>
           <Link
             href={"/wiki"}
-            className="-ml-10 px-2 border-2 rounded-md sm:w-25 md:w-40 lg:w-xl text-center border-brand-turquoise-50 bg-brand-grey-00 text-brand-orange-70"
+            className=" px-2 border-2 rounded-md sm:w-25 md:w-40 lg:w-xl text-center border-brand-turquoise-50 bg-brand-grey-00 text-brand-orange-70"
           >
             Wiki
           </Link>
